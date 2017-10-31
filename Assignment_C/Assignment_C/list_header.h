db963f39b10c7cf3b672ab35f8eb9f09727cae2d
Unknown <kabalydavid@gmail.com>
2017-10-31 13:29:06 +0100

    Megint

--- Assignment_C/Assignment_C/list_header.h
+++ Assignment_C/Assignment_C/list_header.h
@@ -7,8 +7,8 @@ list_t list_create();
 
 void list_dest(list_t self);
 
-int add_item(list_t self, void * item); // Return 0 if item added else -1
-void *get_item(list_t self, uint16_t index); // Return pointer to item at given index in the list
-int no_of_items(list_t self); // Return no of items in list
-int rmv_item(list_t self, void *item); // Return 0 if item removed else -1
+int add_item(list_t self, void * item);
+void *get_item(list_t self, uint16_t index);
+int no_of_items(list_t self);
+int rmv_item(list_t self, void *item);
 
