bb739b90cf3bc6c3ab62c2614e11d491354f397c
Unknown <kabalydavid@gmail.com>
2017-10-31 11:57:02 +0100

    Auto stash before merge of "master" and "origin/master"
    tacher.c added
    header fixes

--- Assignment_C/Assignment_C/teacher_header.h
+++ Assignment_C/Assignment_C/teacher_header.h
@@ -3,9 +3,9 @@
 
 typedef struct teacher *teacher_t;
 
-teacher_t teac_create(char fname[20], int teacher_number);
+teacher_t teach_create(char fname[20], int teacher_num);
 
-void set_teach_fm(teacher_t self, char string[20]);
+void set_teach_fn(teacher_t self, char string[20]);
+char* get_teach_fn(teacher_t self);
 void set_teach_num(teacher_t self, int id);
-char* get_teach_fm(teacher_t self);
 int* get_teach_num(teacher_t self);
